b167d500248a1ef544b0d535ea19b9025d629171
steekam <waynewanyee@gmail.com>
2020-01-24 13:40:03 +0300

    Updated mail configuration to utilize the provided elastic mail api.

--- config/services.php
+++ config/services.php
@@ -30,4 +30,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'elastic_email' => [
+        'key' => env('ELASTIC_KEY'),
+        'account' => env('ELASTIC_ACCOUNT')
+    ]
+
 ];
